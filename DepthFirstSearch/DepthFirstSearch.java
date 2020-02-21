769c016e367d81d8180ce4812ca4aea09b24ca74
Ritvik Dayal <ritvikr1605@gmail.com>
2020-02-21 13:12:40 +0530

    Update DepthFirstSearch.java

--- DepthFirstSearch/DepthFirstSearch.java
+++ DepthFirstSearch/DepthFirstSearch.java
@@ -1,84 +1,70 @@
-package DepthFirstSearch;
-import java.util.List;
-import java.util.Stack;
+import java.io.*; 
+import java.util.*; 
 
+class DepthFirstSearch 
+{ 
+	private int V;
+	private LinkedList<Integer> adj[]; 
 
-public class DepthFirstSearch {
-	
-//	private Stack<Vertex> stack;
-//	
-//	public DepthFirstSearch(){
-//		this.stack=new Stack<>();
-//	}
-//	
-//	public void dfsRecursive(Vertex vertex){
-//		
-//		System.out.print(vertex+"->");
-//		
-//		for( Vertex v : vertex.getNeighbourList() ){
-//			if( !v.isVisited() ){
-//				v.setVisited(true);
-//				dfsRecursive(v);
-//			}
-//		}
-//	}
-//	
-//	public void dfsNormal(Vertex root){
-//		
-//		stack.add(root);
-//		root.setVisited(true);	
-//		
-//		while( !stack.isEmpty() ){
-//			
-//			Vertex actualVertex = stack.pop();
-//			System.out.print(actualVertex+"->");
-//			
-//			for( Vertex v : actualVertex.getNeighbourList() ){
-//				if( !v.isVisited() ){
-//					v.setVisited(true);
-//					stack.push(v);
-//				}
-//			}
-//		}	
-//	}
-	
-	private List<Vertex> vertexList;
-	private int time = 0;
-	
-	public DepthFirstSearch(List<Vertex> vertexList){
-		this.vertexList = vertexList;
-	}
-	
-	public void runDfs(){
-		for( Vertex vertex : vertexList ){
-			if( !vertex.isVisited() ){
-				dfs(vertex);
-			}
-		}
-	}
-	
-	public void dfs(Vertex vertex){
-		
-		System.out.print(vertex.getName()+"-");
-		
-		time++;
-		vertex.setStartingRank(time);
-		
-		for( Vertex v : vertex.getNeighbourList() ){
-			if( !v.isVisited() ){
-				v.setVisited(true);
-				v.setPredecessor(vertex);
-				dfs(v);
-			}
-		}
-		
-		time++;
-		vertex.setFinishingRank(time);	
-	}
-	
-	public void printVertices(){
-		for(Vertex vertex : vertexList){
-			System.out.println(vertex+"");
-		}
-	}
-}
+	DepthFirstSearch(int v) 
+	{ 
+		V = v; 
+		adj = new LinkedList[v]; 
+		for (int i=0; i<v; ++i) 
+			adj[i] = new LinkedList(); 
+	} 
+
+	void addEdge(int v, int w) 
+	{ 
+		adj[v].add(w); // Add w to v's list. 
+	} 
+
+	void DFSUtil(int v,boolean visited[]) 
+	{  
+		visited[v] = true; 
+		System.out.print(v+" "); 
+
+		Iterator<Integer> i = adj[v].listIterator(); 
+		while (i.hasNext()) 
+		{ 
+			int n = i.next(); 
+			if (!visited[n]) 
+				DFSUtil(n, visited); 
+		} 
+	} 
+
+	void DFS(int v) 
+	{ 
+
+		boolean visited[] = new boolean[V]; 
+
+		DFSUtil(v, visited); 
+	} 
+
+	public static void main(String args[]) 
+	{ 
+		Scanner inp = new Scanner(System.in); 
+
+        System.out.println("Enter number of vertices: ");
+        int V = inp.nextInt();
+        System.out.println("Enter number of edges: ");
+        int E = inp.nextInt();
+
+		DepthFirstSearch graph = new DepthFirstSearch(V); 
+
+		for(int i=0; i<E; i++)
+		{
+			System.out.println("Enter source for Edge "+(i+1)+": ");
+			int src = inp.nextInt();
+			System.out.println("Enter destination for Edge "+(i+1)+": ");
+			int des = inp.nextInt();
+			graph.addEdge(src, des);
+		} 
+		System.out.println("Enter Source to start DFS: ");
+		int s = inp.nextInt();
+
+		System.out.println("Following is Depth First Traversal "); 
+
+		graph.DFS(s); 
+	} 
+} 

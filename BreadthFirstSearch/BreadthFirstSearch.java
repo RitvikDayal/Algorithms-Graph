ec20b4d647b15803ed3b6b0edd2d0dcf9978180e
Ritvik Dayal <ritvikr1605@gmail.com>
2020-02-21 13:04:32 +0530

    Update BreadthFirstSearch.java

--- BreadthFirstSearch/BreadthFirstSearch.java
+++ BreadthFirstSearch/BreadthFirstSearch.java
@@ -1,28 +1,77 @@
-package BreadthFirstSearch;
+import java.io.*; 
+import java.util.*; 
+import java.util.Scanner;
 
-import java.util.LinkedList;
-import java.util.Queue;
+class BreadthFirstSearch 
+{ 
+	private int V; // No. of vertices 
+	private LinkedList<Integer> adj[]; //Adjacency Lists 
 
-public class BreadthFirstSearch {
+	BreadthFirstSearch(int v) 
+	{ 
+		V = v; 
+		adj = new LinkedList[v]; 
+		for (int i=0; i<v; ++i) 
+			adj[i] = new LinkedList(); 
+	} 
+ 
+	void addEdge(int v,int w) 
+	{ 
+		adj[v].add(w); 
+	} 
 
-	public void bfs(Vertex root){
-		
-		Queue<Vertex> queue = new LinkedList<>();
-		
-		root.setVisited(true);
-		queue.add(root);
-		
-		while( !queue.isEmpty() ){
-			
-			Vertex actualVertex = queue.remove();
-			System.out.print(actualVertex+" ");
-			
-			for( Vertex v : actualVertex.getNeighbourList() ){
-				if( !v.isVisited() ){
-					v.setVisited(true);
-					queue.add(v);
-				}
-			}			
-		}
-	}
-}
+
+	void BFS(int s) 
+	{ 
+ 
+		boolean visited[] = new boolean[V]; 
+		LinkedList<Integer> queue = new LinkedList<Integer>(); 
+
+		visited[s]=true; 
+		queue.add(s); 
+
+		while (queue.size() != 0) 
+		{ 
+			s = queue.poll(); 
+			System.out.print(s+" "); 
+
+			Iterator<Integer> i = adj[s].listIterator(); 
+			while (i.hasNext()) 
+			{ 
+				int n = i.next(); 
+				if (!visited[n]) 
+				{ 
+					visited[n] = true; 
+					queue.add(n); 
+				} 
+			} 
+		} 
+	} 
+
+	public static void main(String args[]) 
+	{ 
+        Scanner inp = new Scanner(System.in); 
+
+        System.out.println("Enter number of vertices: ");
+        int V = inp.nextInt();
+        System.out.println("Enter number of edges: ");
+        int E = inp.nextInt();
+
+		BreadthFirstSearch graph = new BreadthFirstSearch(V); 
+
+		for(int i=0; i<E; i++)
+		{
+			System.out.println("Enter source for Edge "+(i+1)+": ");
+			int src = inp.nextInt();
+			System.out.println("Enter destination for Edge "+(i+1)+": ");
+			int des = inp.nextInt();
+			graph.addEdge(src, des);
+		} 
+		System.out.println("Enter Source to start BFS: ");
+		int s = inp.nextInt();
+
+		System.out.println("Following is Breadth First Traversal "); 
+
+		graph.BFS(s); 
+	} 
+} 
